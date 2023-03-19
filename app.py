d2e34bfd21820ef19ab9bd034fd3c6e0d839f586
hehwcrashboy <47809978+hehwcrashboy@users.noreply.github.com>
2023-03-20 00:47:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,19 @@
-import requests
-import io
-import base64
-from PIL import Image
 import openai
 import streamlit as st
+from PIL import Image, ImageDraw
+from io import BytesIO
+import base64
 
-# 设置API密钥
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+# Set your API key
+openai.api_key = "your_openai_api_key_here"
 
-# 用于下载图像的函数
-def get_image_download_link(img_data, filename, text):
-    b64 = base64.b64encode(img_data).decode()
-    href = f'<a href="data:file/txt;base64,{b64}" download="{filename}">{text}</a>'
-    return href
+# Streamlit app title
+st.title("Image Generation and Editing with OpenAI")
 
-# 显示生成的图像
-def display_generated_image(url,width=300):
-    response = requests.get(url)
-    img = Image.open(io.BytesIO(response.content))
-    st.image(img, caption="Generated Image", use_column_width=True)
-    img_data = io.BytesIO()
-    img.save(img_data, format="PNG")
-    st.markdown(get_image_download_link(img_data.getvalue(), "generated_image.png", "Download generated image"), unsafe_allow_html=True)
+# Image creation
+st.header("Image Creation")
+image_description = st.text_input("Enter a description to generate an image:")
 
-# 标题和说明
-st.title("Image Generation with OpenAI")
-st.write("Enter a description and generate an image based on the description. You can also upload an image for editing.")
-
-# 文本输入和文件上传
-image_description = st.text_input("Image Description", "A beautiful sunset over the ocean")
-uploaded_file = st.file_uploader("Upload an image for editing (optional)", type=["png", "jpg", "jpeg"])
-
-editing_image = None
-if uploaded_file is not None:
-    editing_image = Image.open(uploaded_file)
-    st.image(editing_image, caption="Uploaded Image", use_column_width=True)
-
-# 生成图像按钮
 if st.button("Generate Image"):
     st.markdown("Generating image...")
 
@@ -50,12 +27,55 @@ if st.button("Generate Image"):
         "response_format": "url",
     }
 
-    if editing_image is not None:
-        img_bytes = io.BytesIO()
-        editing_image.save(img_bytes, format="PNG")
-        base64_encoded_img = base64.b64encode(img_bytes.getvalue()).decode("utf-8")
-        payload["input_image"] = f"data:image/png;base64,{base64_encoded_img}"
-
     response = openai.Image.create(**payload)
     generated_image_url = response["data"][0]["url"]
-    display_generated_image(generated_image_url)
+    st.image(generated_image_url, caption="Generated Image", width=300)
+    st.markdown(get_image_download_link(generated_image_url, "generated_image.png", "Download generated image"), unsafe_allow_html=True)
+
+# Image editing with mask
+st.header("Image Editing with Mask")
+uploaded_image = st.file_uploader("Upload an image for editing:")
+
+def get_image_download_link(img_url, filename, text):
+    img_data = requests.get(img_url).content
+    b64 = base64.b64encode(img_data).decode()
+    return f'<a href="data:image/png;base64,{b64}" download="{filename}">{text}</a>'
+
+if uploaded_image is not None:
+    original_image = Image.open(uploaded_image)
+    st.image(original_image, caption="Original Image", width=300)
+
+    mask_color = st.color_picker("Select a mask color:", value="#FF0000")
+    x1 = st.number_input("Mask X1 position:", value=0)
+    y1 = st.number_input("Mask Y1 position:", value=0)
+    x2 = st.number_input("Mask X2 position:", value=100)
+    y2 = st.number_input("Mask Y2 position:", value=100)
+
+    if st.button("Edit Image with Mask"):
+        # Create a copy of the original image to draw the mask
+        image_with_mask = original_image.copy()
+        draw = ImageDraw.Draw(image_with_mask)
+        draw.rectangle([x1, y1, x2, y2], fill=mask_color)
+        st.image(image_with_mask, caption="Image with Mask", width=300)
+
+        # Convert the image with mask to base64
+        buffered = BytesIO()
+        image_with_mask.save(buffered, format="PNG")
+        base64_encoded_image_with_mask = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+        edit_description = st.text_input("Enter a description to edit the image with mask:")
+        edit_payload = {
+            "image": base64_encoded_image_with_mask,
+            "description": edit_description,
+            "model": "image-alpha-001",
+        }
+
+        edit_response = openai.Image.create_edit(**edit_payload)
+        edited_image_url = edit_response["url"]
+
+        st.image(edited_image_url, caption="Edited Image", width=300)
+        st.markdown(get_image_download_link(edited_image_url, "edited_image_with_mask.png", "Download edited image with mask"), unsafe_allow_html=True
+# Request frequency limit
+st.write("## Request Frequency Limit")
+requests_per_minute = st.number_input("Enter maximum requests per minute:", min_value=1, value=10)
+st.write("Limit your app to ", requests_per_minute, " requests per minute.")
